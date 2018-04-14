1ac5562499f9e8ab2085f21eca6ec39be79ae0ea
souhmaa <maalaoui.ENSI@gmail.com>
2018-04-14 18:27:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('This is my first Docker node container');
 });
 
 app.listen(PORT, HOST);
