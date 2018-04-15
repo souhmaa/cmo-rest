5ce8e6d6b698ae067b0e5304731490f48dd2a879
souhmaa <maalaoui.ENSI@gmail.com>
2018-04-15 12:24:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,20 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  let message = `MAIL = ${process.env.RDS_PASSWORD}\n`;
-  message += `MAIL = ${process.env.AWS_ACCESS_KEY_ID}\n`;
-  message += `MAIL = ${process.env.AWS_SECRET_ACCESS_KEY}\n`;
-  message += `MAIL = ${process.env.EMAIL_ADDRESS}\n`;
-  message += `MAIL = ${process.env.EMAIL_IMAP}\n`;
-  message += `MAIL = ${process.env.EMAIL_PASSWORD}\n`;
-  message += `MAIL = ${process.env.EMAIL_SMTP}\n`;
-  message += `MAIL = ${process.env.OCR_LICENSE_CODE}\n`;
-  message += `MAIL = ${process.env.OCR_USERNAME}\n`;
-  message += `MAIL = ${process.env.RDS_DATABASE}\n`;
-  message += `MAIL = ${process.env.RDS_HOST}\n`;
-  message += `MAIL = ${process.env.RDS_PORT}\n`;
-  message += `MAIL = ${process.env.RDS_USER}\n`;
-  res.send(message);
+  res.send('message');
 });
 
 app.listen(PORT, HOST);
